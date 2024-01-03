dc90dca9d40edd2940a62b199349c33428bb2234
Saif Ali <kattan.saifali@gmail.com>
2024-01-03 21:36:56 +0100

    add: third exco completed

---
+++ src/test/Exco03.java
@@ -0,0 +1,17 @@
+package test;
+
+
+// Afficher le périmètre d’un cercle dont le rayon est une variable 
+
+public class Exco03 {
+	
+	public static void main(String[] args) {
+	
+	int perimetreCricle = 2 * 4;
+	double chiffreP = 3.14d;
+	
+	System.out.print(perimetreCricle * chiffreP);
+	
+	}	
+
+}
