48d0daa58f8a4f9f57c65d8ebb7916569908c769
Saif Ali <kattan.saifali@gmail.com>
2024-01-03 21:36:25 +0100

    add: second  exco completed

---
+++ src/test/Exco02.java
@@ -0,0 +1,13 @@
+package test;
+
+// Reprendre l’exercice n°1 en utilisant une variable
+
+public class Exco02 {
+	public static void main(String[] args) {
+		
+		String helloWorld = "Hello World";
+		
+		System.out.print(helloWorld);
+	}
+
+}
