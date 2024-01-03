86314d2197fcc21e978fccb0c9ef0af35a209d45
Saif Ali <kattan.saifali@gmail.com>
2024-01-03 21:35:51 +0100

    add: first exco completed

---
+++ src/test/Exco01.java
@@ -0,0 +1,12 @@
+package test;
+
+// Afficher le texte Hello World dans la console de sortie
+
+public class Exco01 {
+	public static void main(String[] args) {
+		
+		System.out.print("Hello World");
+		
+	}
+
+}
